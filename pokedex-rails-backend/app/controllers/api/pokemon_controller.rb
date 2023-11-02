4219b2e7d30d88e56cdb27f0c2a30e2d8fba9a8a
Akash Pulluru <akashpulluru@gmail.com>
2023-11-02 17:00:18 -0400

    ending

--- pokedex-rails-backend/app/controllers/api/pokemon_controller.rb
+++ pokedex-rails-backend/app/controllers/api/pokemon_controller.rb
@@ -1,8 +1,17 @@
 class Api::PokemonController < ApplicationController
     def index
+        @pokemon = Pokemon.all 
+        render json: @pokemon 
     end
 
     def create
+        pokemon = Pokemon.new(pokemon_params)
+
+        if pokemon.save 
+            render json: pokemon 
+        else
+            render json: pokemon.errors.full_messages, status: 422  
+        end 
     end
 
     def types
@@ -29,11 +38,17 @@ class Api::PokemonController < ApplicationController
     end
 
     def show
+        pokemon = Pokemon.find(params[:id])
+        render json: pokemon 
     end
 
-    def update
-    end
+    # def update
+    # end
 
-    def destroy
-    end
+    # def destroy
+    # end
+
+    def pokemon_params 
+        params.require(:pokemon).permit(:number, :name, :attack, :defense, :poke_type, :image_url, :captured)
+    end 
 end
