6ec7e272b50d92309f041c2886a8e91fcf1f5529
Neel <nkachalia1@gmail.com>
2023-11-02 11:50:10 -0400

    validations

---
+++ pokedex-rails-backend/app/models/pokemon.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: pokemons
+#
+#  id         :bigint           not null, primary key
+#  number     :integer          not null
+#  name       :string           not null
+#  attack     :integer          not null
+#  defense    :integer          not null
+#  poke_type  :string           not null
+#  image_url  :string           not null
+#  captured   :boolean          default(FALSE), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Pokemon < ApplicationRecord
+    validates :id, :number, :name, :attack, :defense, :poke_type, :image_url, :captured, presence: true
+end
